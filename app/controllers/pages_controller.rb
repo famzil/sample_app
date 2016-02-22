9a47c5e67e787baa2b6d6045613d46c597733b10
famzil <fatimaamzil93@gmail.com>
2016-02-22 23:19:09 +0100

    Fini avec les pages statiques :D

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title="Accueil" #title is a variable instance
   end
 
   def contact
+    @title="Contact"
+  end
+
+  def about
+    @title="A Propos"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views  # to tell RSpec to render views inside tests of control
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    it "should be the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "Simple App du Tutoriel Ruby on Rails | Accueil")
     end
   end
 
@@ -14,6 +20,23 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should be the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content => "Simple App du Tutoriel Ruby on Rails | Contact")
+    end
   end
+
+  describe "GET 'about'" do
+   it "should be successful" do
+     get 'about'
+     response.should be_success
+   end
+   it "should be the right title" do
+     get 'about'
+     response.should have_selector("title",
+                       :content => "Simple App du Tutoriel Ruby on Rails | A Propos")
+   end
+ end
 
 end
