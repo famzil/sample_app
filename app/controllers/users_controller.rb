bbd62da143aa17b424f69c2636ad8e01e7b27898
famzil <fatimaamzil93@gmail.com>
2016-03-06 17:06:19 +0100

    Inscription utilisateur finie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,20 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user=User.new
     @title = "Signup"
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Traite un succès d'enregistrement.
+      flash[:success] = "Wellcome to the Sample Application!"
+      redirect_to @user
+    else
+      @title = "Signup"
+      render 'new'
+    end
   end
 
 end
